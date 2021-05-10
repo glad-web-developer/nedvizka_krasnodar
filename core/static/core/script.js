7b5e06410472a37c23c1a804e5dbda610eb6e4ac
glad-web-developer <glad-web-developer@gmail.com>
2021-05-10 16:40:10 +0900

    подсветка активного меню

--- core/static/core/script.js
+++ core/static/core/script.js
@@ -12,26 +12,35 @@ function vivisti_uvedomlenie(zagolovok, telo, type) {
 
     $('#uvedomlenie').toast('show');
 }
+
+
+function podsvetit_aktivnoe_menu() {
+    $('.main_menu').find('.nav-link').each(function () {
+        if ((window.location.href.indexOf( $(this).attr('href'))) > -1) {
+            $(this).addClass('active');
+        }
+    });
+}
+
 //
 $(() => {
 
 
+    $(document).ready(function () {
 
-     $(document).ready(function () {
-
-
-            $('.zakritie_sidbara, main').on('click', function () {
-                $('.my_sidebar').removeClass('active');
-                // $('.overlay').removeClass('active');
-            });
-
-            $('.otkritie_sidbara').on('click', function () {
-                $('.my_sidebar').toggleClass('active');
-                // $('.overlay').addClass('active');
-                // $('.collapse.in').toggleClass('in');
-                // $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-            });
+        podsvetit_aktivnoe_menu();
+        $('.zakritie_sidbara, main').on('click', function () {
+            $('.my_sidebar').removeClass('active');
+            // $('.overlay').removeClass('active');
         });
+
+        $('.otkritie_sidbara').on('click', function () {
+            $('.my_sidebar').toggleClass('active');
+            // $('.overlay').addClass('active');
+            // $('.collapse.in').toggleClass('in');
+            // $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+    });
 
 
 });
